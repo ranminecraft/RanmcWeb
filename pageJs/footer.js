e66ce292e7faa61f5804bef868bf29b574825599
Ran <xyfwdy@gmail.com>
2024-03-02 07:58:35 +0800

    重构 banlist页面

---
+++ pageJs/footer.js
@@ -0,0 +1,35 @@
+document.writeln(`
+<!-- Footer -->
+<div class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="text-container about">
+                    <h4>关于更多</h4>
+                    <p style="color:#dfe5ec;margin-bottom:0rem;">运营By <a
+                            href="http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=LE3avMylzVe1aniqZ_mvxEc1EzwhYyLd&authKey=%2F1jA8wis%2F1N31FYwZ2Gp%2B52kRBZKnJrAaRMxOX">阿然</a></p>
+                    <p style="color:#dfe5ec;margin-bottom:0rem;">美工By <a
+                            href="http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=LE3avMylzVe1aniqZ_mvxEc1EzwhYyLd&authKey=%2F1jA8wis%2F1N31FYwZ2Gp%2B52kRBZKnJrAaRMxOX">阿然</a></p>
+                    <p style="color:#dfe5ec;margin-bottom:0rem;">建筑By <a
+                            href="http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=LE3avMylzVe1aniqZ_mvxEc1EzwhYyLd&authKey=%2F1jA8wis%2F1N31FYwZ2Gp%2B52kRBZKnJrAaRMxOX">光圈</a></p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Copyright -->
+<div class="copyright">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <p style="color:#dfe5ec;margin-bottom:0rem"> 友情外链&nbsp;<a target="_blank"
+                        href="https://minecraft.net/">Minecraft</a>&nbsp;&nbsp;<a target="_blank"
+                        href="https://minepay.top/">MinePay</a>&nbsp;&nbsp;<a target="_blank"
+                        href="https://www.anzifan.com/">AnZiFan</a></p>
+                <p class="p-small">MIT License Copyright &copy; 2023 <a target="_blank"
+                        href="https://github.com/ranminecraft/Ranmcweb">阿然姐姐丨版权所有</a></p>
+            </div> <!-- end of col -->
+        </div> <!-- enf of row -->
+    </div> <!-- end of container -->
+</div> <!-- end of copyright -->
+`)
