a414eaa9a1c8198e35103107f06b6de549317866
Ran <xyfwdy@gmail.com>
2025-07-02 12:31:45 +0800

    更新加群链接

--- constant/groupChat.js
+++ constant/groupChat.js
@@ -1,3 +1,3 @@
 document.writeln(`
-<a class="btn-solid-reg page-scroll" href="https://qun.qq.com/universal-share/share?ac=1&authKey=MLHOTkfkfj7S6Cj319OMoiNDX9ADNduKqvSnlY3cZpWUUobPNGARQPT6K7MOq5qq&busi_data=eyJncm91cENvZGUiOiI1Nzk4ODI1MDUiLCJ0b2tlbiI6Ik5VbHc5QWlLWEkrd2I4QmVFUzFvTEFRMW1SL1JRNTdGdkpmVFlWWWdaNkVzYkMwL2FZcXVwRW5PS3hORE" target="_blank">点击加群</a></h3>
+<a class="btn-solid-reg page-scroll" href="https://qm.qq.com/q/vKrsJNZsNG" target="_blank">点击加群</a></h3>
 `)
