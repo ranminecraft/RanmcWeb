1388262825470ccc00b54137b8aac9358dd54d87
阿然 <69817107+ranminecraft@users.noreply.github.com>
2025-09-15 00:59:57 +0800

    Update header.js

--- constant/header.js
+++ constant/header.js
@@ -28,26 +28,12 @@ document.writeln(`    <nav class="navbar navbar-expand-md navbar-dark navbar-cus
                     <a class="nav-link page-scroll" href="news.html">游戏公告</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link page-scroll" href="banlist.html">封禁列表</a>
+                    <a class="nav-link page-scroll" href="https://gitee.com/xyfwdy/ranmc/issues" target="_blank">举报反馈</a>
                 </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle page-scroll" id="navbarDropdown" role="button"
-                        aria-haspopup="true" aria-expanded="false">友情外链</a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="https://minecraft.net/"><span
-                                class="item-text">Minecraft</span></a>
-                        <div class="dropdown-items-divide-hr"></div>
-                        <a class="dropdown-item" href="https://minepay.top/"><span class="item-text">MinePay</span></a>
-                        <div class="dropdown-items-divide-hr"></div>
-                        <a class="dropdown-item" href="https://minelive.top/"><span class="item-text">MineLive</span></a>
-                        <div class="dropdown-items-divide-hr"></div>
-                        <a class="dropdown-item" href="https://www.anzifan.com/"><span
-                                class="item-text">AnZiFan</span></a>
-                        <div class="dropdown-items-divide-hr"></div>
-                        <a class="dropdown-item" href="https://www.minepay.top/"><span
-                                class="item-text">矿世问答</span></a>
-                    </div>
+                <li class="nav-item">
+                    <a class="nav-link page-scroll" href="banlist.html">封禁列表</a>
                 </li>
             </ul>
         </div>
+
     </nav>    `)
